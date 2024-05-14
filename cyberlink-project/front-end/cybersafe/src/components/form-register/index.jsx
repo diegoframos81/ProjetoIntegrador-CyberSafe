de6d6ef255363d06bcb94105129685ae68a97e42
Diego Francisco Ramos <89109205+diegoframos81@users.noreply.github.com>
2024-05-14 17:22:03 -0300

    fix: adjusting syle/route/functions

--- cyberlink-project/front-end/cybersafe/src/components/form-register/index.jsx
+++ cyberlink-project/front-end/cybersafe/src/components/form-register/index.jsx
@@ -1,28 +1,60 @@
-
-import { Input, Select, Button, FormContainer, } from './style';
+import React, { useState } from 'react';
+import { Input, Select, Button, FormContainer } from './style';
 import { useNavigate } from "react-router-dom";
 
-
 export function Form() {
+
     const navigate = useNavigate();
+    
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [confirmarSenha, setConfirmarSenha] = useState('');
+    const [tipo, setTipo] = useState(''); 
+    const [returnMsg, setMsg] = useState('');
+
+    const MakeJsonUserAdd = () => {
+
+        const userAdd = {
+            Name: nome,
+            Email: email,
+            Password: senha,
+            TypeUser: tipo,
+            confirmePassword: confirmarSenha,
+        };
+
+        return userAdd;
+    };
+
+    const HandleRegistro  = () => {
+        var userAdd = MakeJsonUserAdd();
+        if(userAdd.Password != userAdd.confirmePassword){
+            setMsg('Senhas divergentes. Por favor, verifique confirme a senha');
+            $('#alert-erro').removeClass('hide');
+        }else{
+            axios.post('http://localhost:8080/InsertNewUser', userAdd).then(response => {
+                navigate("/");
+                $('#alert-erro').addClass('hide');
+            }).catch(error => { 
+                setMsg('Houve um erro ao cadastrar o usuário');
+                $('#alert-erro').removeClass('hide');
+            });
+        }
+    };
+
     return (
         <FormContainer>
+            <div id="alert-erro" className="alert alert-danger hide" role="alert">{returnMsg}</div>
 
-            <div id="alert-erro" style={{ width: "100%", height: "70px" }} className="alert alert-danger hide" role="alert">{returnMsg}</div>
-
-
-            <Input placeholder="Nome" />
-            <Input placeholder="E-mail" />
-            <Input placeholder="Senha" />
-            <Input placeholder="Confirme a senha" />
-            <Select>
-                <option>SELECT</option>
-                <option>Responsável</option>
-                <option>Educador</option>
-                
+            <Input placeholder="Nome" value={nome} onChange={e => setNome(e.target.value)} />
+            <Input placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} />
+            <Input placeholder="Senha" type="password" value={senha} onChange={e => setSenha(e.target.value)} />
+            <Input placeholder="Confirme a senha" type="password" value={confirmarSenha} onChange={e => setConfirmarSenha(e.target.value)} />
+            <Select value={tipo} onChange={e => setTipo(e.target.value)}>
+                <option value="1">Responsável</option>
+                <option value="2">Educador</option>
             </Select>
-            <Button onClick={() => {{ navigate("/") }}}>Registrar</Button>
-            
+            <Button type="button" onClick={HandleRegistro}>Registrar</Button>
         </FormContainer>
     );
 }
