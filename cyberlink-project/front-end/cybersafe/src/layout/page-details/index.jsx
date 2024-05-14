f54a1ce837599d767016a0bb612bb41d063cb18f
Diego Francisco Ramos <89109205+diegoframos81@users.noreply.github.com>
2024-05-14 13:32:02 -0300

    FIX

--- cyberlink-project/front-end/cybersafe/src/layout/page-details/index.jsx
+++ cyberlink-project/front-end/cybersafe/src/layout/page-details/index.jsx
@@ -3,7 +3,7 @@ import ChartComponent from '../../components/gráfico-notas/gráfico/index';
 import CardContainer from '../../components/card-disciplinas/card-container/index';
 import CardNotas from '../../components/card-notas/index';
 
-import { ContainerDetails } from '../page-details/style';
+import { ContainerDetails } from './style';
 import CardCoeficiente from "../../components/card-coef";
 
 export const DetailsPage = () => {
