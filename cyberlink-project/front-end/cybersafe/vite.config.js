95a6744dd9dd14e812e58e000bce1bb2abe10328
Diego Francisco Ramos <89109205+diegoframos81@users.noreply.github.com>
2024-05-28 14:48:41 -0300

    feat: add vite-config

--- cyberlink-project/front-end/cybersafe/vite.config.js
+++ cyberlink-project/front-end/cybersafe/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/students': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/students/, '')
+      },
+      // Adicione mais endpoints conforme necessário
+    }
+  }
 })
